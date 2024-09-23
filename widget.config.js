9077b02ac5d7b83ca3656c3be6e23cdf16f38e40
urchin80 <31174107+urchin80@users.noreply.github.com>
2024-09-23 17:50:33 +0800

    Initial commit

---
+++ widget.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  widgetName: "jsDev",
+  server: "$",
+  file: "jsDev",
+  uploadScript: "UploadToHTML",
+  reloadScript: "Load Widget",
+  startDevScript: "Set to Dev Mode",
+};
